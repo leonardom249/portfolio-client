27f478bf57af3dec03901d0dd049c9603bd3ef7d
leonardom249 <leonardo.megan@gmail.com>
2018-05-13 20:04:06 -0500

    made nav bar into separate component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { WorkExamples } from './components/work-examples';
+import { NavBar } from './components/nav-bar';
 
 class App extends Component {
   render() {
     return (
       <div className="portfolio app main">
       <h1>Megan Leonardo's Portfolio</h1>
-      <h2>
-        <a className="nav" href="https://github.com/leonardom249">Github</a>
-        <a className="nav" href="https://www.linkedin.com/in/meganleonardo/">Linkedin</a>
-        <a className="nav" href="https://github.com/leonardom249/portfolio-server">Portfolio Server Code</a>
-        <a className="nav" href="https://github.com/leonardom249/portfolio-client.git">and Client Code</a>
-      </h2>
-        <WorkExamples/>
+      <NavBar/>
+      <WorkExamples/>
       </div>
     );
   }
---
+++ src/components/nav-bar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function NavBar (){
+    return(
+       <nav>
+            <h2>
+                <a className="nav" href="https://github.com/leonardom249">Github</a>
+                <a className="nav" href="https://www.linkedin.com/in/meganleonardo/">Linkedin</a>
+                <a className="nav" href="https://github.com/leonardom249/portfolio-server">Portfolio Server Code</a>
+                <a className="nav" href="https://github.com/leonardom249/portfolio-client.git">and Client Code</a>
+            </h2>
+       </nav>
+    )
+}
