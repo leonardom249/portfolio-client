961a6865c3095039d8f6d7aaaa383b8f09a7bd0d
leonardom249 <leonardo.megan@gmail.com>
2018-05-13 20:01:30 -0500

    made examples into a separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { WorkExamples } from './components/work-examples';
 
 class App extends Component {
   render() {
@@ -9,30 +10,10 @@ class App extends Component {
       <h2>
         <a className="nav" href="https://github.com/leonardom249">Github</a>
         <a className="nav" href="https://www.linkedin.com/in/meganleonardo/">Linkedin</a>
-        <a className="nav" href="https://github.com/leonardom249/portfolio-server">Portfolio Server</a>
-        <a className="nav" href="https://github.com/leonardom249/portfolio-client.git">and Client</a>
+        <a className="nav" href="https://github.com/leonardom249/portfolio-server">Portfolio Server Code</a>
+        <a className="nav" href="https://github.com/leonardom249/portfolio-client.git">and Client Code</a>
       </h2>
-
-      <section>
-        <div className="work-examples">
-          <div className="example-1">
-            <h3>Ingredient Inheritance</h3>
-            <ul>
-                <li> <a href="https://github.com/leonardom249/ingredient-inheritance-server">Server Code</a></li>
-                <li> <a href="https://github.com/leonardom249/ingredient-inheritance-client">Client Code</a></li>
-            </ul>
-            <h4>Click Picture for Deployed Version:</h4>
-            <a href='https://vigilant-leakey-ceaa26.netlify.com/'>
-             <img alt="ingredient-inheritance-deployed" src='https://lh3.googleusercontent.com/n-dayesQno7wI38X0_qEyUYp0BYgAzvKKAeCcqb9ryiAQ9RKw577u-WAwFMrVU5IZ4s3I5skL9IaJItvYXvshGzZ2_r813yQYXByvNgYph8mrF735VN75yvqTk6Mia7-p6oU5fvB=w2400' />
-            </a> 
-          </div>
-        </div>
-        <div className="work-examples"></div>
-        <div className="work-examples"></div>
-        <div className="work-examples"></div>
-        <div className="work-examples"></div>
-        <div className="work-examples"></div>
-      </section>
+        <WorkExamples/>
       </div>
     );
   }
---
+++ src/components/work-examples.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function WorkExamples (){
+    return(
+        <div className="examples"> 
+            <div className="work-example">
+            <div className="example-1">
+                <h3>Ingredient Inheritance</h3>
+                <ul>
+                    <li> <a href="https://github.com/leonardom249/ingredient-inheritance-server">Server Code</a></li>
+                    <li> <a href="https://github.com/leonardom249/ingredient-inheritance-client">Client Code</a></li>
+                </ul>
+                <h4>Click Picture for Deployed Version:</h4>
+                <a href='https://vigilant-leakey-ceaa26.netlify.com/'>
+                <img alt="ingredient-inheritance-deployed" src='https://lh3.googleusercontent.com/n-dayesQno7wI38X0_qEyUYp0BYgAzvKKAeCcqb9ryiAQ9RKw577u-WAwFMrVU5IZ4s3I5skL9IaJItvYXvshGzZ2_r813yQYXByvNgYph8mrF735VN75yvqTk6Mia7-p6oU5fvB=w2400' />
+                </a> 
+            </div>
+            </div>
+            <div className="work-example"></div>
+            <div className="work-example"></div>
+            <div className="work-example"></div>
+            <div className="work-example"></div>
+            <div className="work-example"></div>
+        </div>
+    )
+}
