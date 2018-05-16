549ea671d63a052a37055d7450884f0776b42f65
leonardom249 <leonardo.megan@gmail.com>
2018-05-15 21:05:45 -0400

    added more basic css

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../component-css/nav.css';
 
 export function NavBar (){
     return(
@@ -7,8 +8,6 @@ export function NavBar (){
                 <a className="nav" href="https://docs.google.com/document/d/1LQXfYq6V0Majp8OrzevKYLiB6VUXM5e4O5H9-YvEStQ/edit?usp=sharing">Resume</a>
                 <a className="nav" href="https://github.com/leonardom249">Github</a>
                 <a className="nav" href="https://www.linkedin.com/in/meganleonardo/">Linkedin</a>
-                {/* <a className="nav" href="https://github.com/leonardom249/portfolio-server">Portfolio Server Code</a> */}
-                <a className="nav" href="https://github.com/leonardom249/portfolio-client.git">Portfolio Code</a>
             </h2>
        </nav>
     )
--- src/components/work-examples.js
+++ src/components/work-examples.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../component-css/work-examples.css';
 
 export function WorkExamples (){
     return(
