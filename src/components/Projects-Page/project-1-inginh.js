35d431b7b4b5612b83e656c413614cafe1176299
leonardom249 <leonardo.megan@gmail.com>
2018-07-02 16:58:12 -0500

    adding ing inh page

--- src/components/Projects-Page/project-1-inginh.js
+++ src/components/Projects-Page/project-1-inginh.js
@@ -8,18 +8,19 @@ export default class IngredientInheritance extends React.Component {
         return (
             <ScrollableAnchor id={'ing-inh'}>
                 <section className="project-section background-ing-inh">
-                    <h2 className="title">Project 1: Ingredient Inheritance</h2>
-                    <div className="text-container row">
+                    <h2 className="title">Ingredient Inheritance</h2>
+                    <div className="buttons-container">
                         <div className="project-left col-6">
-                            <a href="https://github.com/leonardom249/ingredient-inheritance-client" className="projects-button">Github Client</a>
+                            <a href="https://github.com/leonardom249/ingredient-inheritance-client" className="ing-inh-button">Github Client</a>
                         </div>
                         <div className="project-right col-6">
-                        <a href="https://github.com/leonardom249/ingredient-inheritance-server" className="projects-button">Github Server</a>
+                        <a href="https://github.com/leonardom249/ingredient-inheritance-server" className="ing-inh-button">Github Server</a>
                         </div>
                     </div>
                     <div className="row">
                         <div className="buttons-container">
-                            <a href='#top' className="scroll-top-button">Scroll Top</a>
+                            <a href='#top' className="move-button">Back to The Top</a>
+                            <a href='#hands-to-hear' className="move-button">Next</a>
                         </div>
                     </div>
                 </section>
