d17f4b1cc6e826fad1f8e79221ed73ec7a3a8cf1
leonardom249 <leonardo.megan@gmail.com>
2018-07-10 13:45:36 -0500

    changed hth demo account

--- src/components/Projects-Page/project-2-hth.js
+++ src/components/Projects-Page/project-2-hth.js
@@ -13,7 +13,7 @@ export default class HandsToHear extends React.Component {
                         <p className="sub-title">About App: </p>
                         <p className="app-info">Hands to Hear was created as a way to work on my knowledge of data structures and algorithms while also providing an easy way for someone to learn a new, and very important, language.  HTH uses the MERN stack and spaced repetition to teach the most basic, but integral part of American Sign Language: the alphabet.  The app displays pictures of signed letters with a textbox for the users answer.  If the user is correct, they will move on faster and the correct letter will appear less frequently.  If the user is incorrect, the letter will appear more frequently until correct.  This app was created with another full stack developer, Alexa Scott.</p>
                         <p className="sub-title demo-account">Deployed Demo Account:</p>
-                        <p className="about-info"><strong> Username:<em> Demo</em> <br/> Password:<em> demo1234</em></strong></p> 
+                        <p className="about-info"><strong> Username:<em> demo</em> <br/> Password:<em> demo1234</em></strong></p> 
                         <div className="buttons-container">
                             <a href="https://hands-to-hear.netlify.com/" target="_blank" rel="noopener noreferrer" className="action-2-button">Deployed App <i className="fas fa-hands"></i></a>
                             <a href="https://github.com/leonardom249/hands-to-hear-client" target="_blank" rel="noopener noreferrer" className="action-2-button">Github Client <i className="fab fa-github"></i></a>
